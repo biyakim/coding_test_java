5ba1dae18d5bb6737ba625f5034dbe0887679983
biyakim <w2116@e-mirim.hs.kr>
2022-12-29 10:13:28 +0900

    람다식 풀기!

---
+++ src/Timer2.java
@@ -0,0 +1,16 @@
+public class Timer {
+    public static void main(String[] args) {
+        Timer t = new Timer(1000,event -> System.out.println("beep"));
+        t.start();
+
+        for(int i = 0;i<1000;i++){
+            try{
+                Thread.sleep(1000);
+
+
+                }catch(InterruptedException e){
+            }
+        }
+
+    }
+}
